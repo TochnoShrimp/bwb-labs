552d01cfff86c13d73043b97c48992900af5ca85
Shrimp <sasa-boss.dimurin@yandex.ru>
2026-04-18 22:56:10 +0400

    Delete feature/lab3-typescript/backend/dist directory

--- feature/lab3-typescript/backend/dist/models/index.js
+++
@@ -1,7 +0,0 @@
-import sequelize from '../config/db.js';
-import User from './User.js';
-import Event from './Event.js';
-import BlacklistedToken from './BlacklistedToken.js';
-User.hasMany(Event, { foreignKey: 'createdBy' });
-Event.belongsTo(User, { foreignKey: 'createdBy' });
-export { sequelize, User, Event, BlacklistedToken };
